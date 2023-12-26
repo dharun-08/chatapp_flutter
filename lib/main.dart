71483814dc11ea6989cfe86e150dfdd9c2dadc3c
dharun-08 <dharun08kumar@gmail.com>
2023-12-26 17:17:58 +0530

    firebase committ

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 // import 'package:chat_app/pages/login_page.dart';
-import 'package:chat_app/pages/register_page.dart';
-import 'package:chat_app/services/auth/login_or_register.dart';
+// import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/services/auth/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:chat_app/services/auth/login_or_register.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -13,10 +18,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  LogInorRegister(),
+      home: AuthGate(),
     );
   }
 }
-
